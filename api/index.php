1d34ff025113190f529a4862e40ead24d1ea0c01
RianHarahap <rianharahap23@gmail.com>
2026-01-21 17:06:42 +0700

    vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,20 @@
 <?php
 
+// VERCEL CONFIGURATION : Setup /tmp environment
+
+// 1. Setup SQLite Database in /tmp (Read-Only fix)
+$dbSource = __DIR__ . '/../database/database.sqlite';
+$dbDest = '/tmp/database.sqlite';
+
+if (!file_exists($dbDest)) {
+    // Jika ada file database di source, copy dulu
+    if (file_exists($dbSource)) {
+        copy($dbSource, $dbDest);
+    } else {
+        // Jika tidak ada, buat file kosong
+        touch($dbDest);
+    }
+}
+
+// 2. Delegate to standard Laravel public/index.php
 require __DIR__ . '/../public/index.php';
